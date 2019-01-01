6dd051302e2358c9ce204477d13cfd50411a9433
Seth Holladay <me@seth-holladay.com>
2019-01-01 05:54:41 -0500

    Add game board component

---
+++ renderer/component/board.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const WallRow = () => {
+    return new Array(17).fill('_temp').map((elem, index) => {
+        // eslint-disable-next-line react/no-array-index-key
+        return <div key={index} className="wall-space" />;
+    });
+};
+
+const MixedRow = () => {
+    return new Array(17).fill('_temp').map((elem, index) => {
+        const isEven = (index % 2) === 0;
+        const type = isEven ? 'pawn' : 'wall';
+        // eslint-disable-next-line react/no-array-index-key
+        return <div key={index} className={`${type}-space`} />;
+    });
+};
+
+const Board = () => {
+    const rows = new Array(17).fill('_temp').map((elem, index) => {
+        const isEven = (index % 2) === 0;
+        const Row = isEven ? MixedRow : WallRow;
+        // eslint-disable-next-line react/no-array-index-key
+        return <Row key={index} />;
+    });
+    return (
+        <>
+            <div id="board">
+                {rows}
+            </div>
+            <style jsx>{`
+                #board {
+                    display: grid;
+                    grid-template-columns: repeat(8, 10% 1fr) 10%;
+                    grid-template-rows: repeat(8, 10% 1fr) 10%;
+                    width: 50vw;
+                    min-width:400px;
+                    height: 50vw;
+                    min-height:400px;
+                    border: 5px solid black;
+                    margin-left: auto;
+                    margin-right: auto;
+                }
+                .pawn-space {
+                    background: hsl(40, 50%, 50%);
+                }
+                .wall-space {
+                    background: hsl(40, 85%, 30%);
+                }
+            `}
+            </style>
+        </>
+    );
+};
+
+export default Board;
--- renderer/pages/index.jsx
+++ renderer/pages/index.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Button } from 'semantic-ui-react';
 import Layout from '../layout/website.jsx';
+import Board from '../component/board.jsx';
 import PlayersForm from '../component/players-form.jsx';
+
+const Game = (props) => {
+    return (
+        <>
+            <Board />
+            <Button onClick={props.onAbort}>End game</Button>
+        </>
+    );
+};
+Game.propTypes = {
+    onAbort : PropTypes.func.isRequired
+};
 
 const Index = () => {
     const [isInGame, setIsInGame] = useState(false);
@@ -18,7 +32,7 @@ const Index = () => {
             <h1>Quoridor</h1>
             <p>{isInGame ? 'Running game...' : 'An abstract strategy game'}</p>
             {isInGame ?
-                <Button onClick={handleAbort}>End game</Button> :
+                <Game onAbort={handleAbort} /> :
                 <PlayersForm onSubmit={handleStart} />
             }
         </Layout>
